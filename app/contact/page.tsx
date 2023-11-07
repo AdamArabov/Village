e4bb22f968dc335e9c3c974f7932e82e2b4d50e2
AdamArabov <arabovadam@yahoo.com>
2023-11-07 13:34:18 -0500

    mobile view for contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,21 +1,21 @@
 import ContactForm from "../component/ContactInfo/ContactForm";
 import Operations from "../component/ContactInfo/Operation";
 import Map from "../component/Map/Map";
-
-export default function Contact(){
-    return(
-<div className="pt-4">
-  <div className="flex">
-    <div className="w-screen md:w-1/2 lg:w-1/2">
-      <Map />
-    </div>
-    <div className="w-screen md:w-1/2 lg:w-1/2 pt-10">
-      <ContactForm />
-    </div>
-  </div>
-  <div className="pt-10 pb-4">
-    <Operations />
-  </div>
-</div>
-    )
-}
+export default function Contact() {
+    return (
+      <div className="pt-4">
+        <div className="flex flex-col md:flex-row lg:flex-row">
+          <div className="w-full md:w-1/2 lg:w-1/2 md:mb-0 lg:mb-0 mb-10">
+            <Map />
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/2 md:pt-0 lg:pt-0 pt-10">
+            <ContactForm />
+          </div>
+        </div>
+        <div className="pt-20 pb-4">
+          <Operations />
+        </div>
+      </div>
+    );
+  }
+  
