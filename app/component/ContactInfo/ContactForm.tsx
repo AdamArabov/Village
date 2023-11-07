c40aa1ef43a0557c51d4035635a147de65fa8909
AdamArabov <arabovadam@yahoo.com>
2023-11-07 12:39:59 -0500

    Working Form

---
+++ app/component/ContactInfo/ContactForm.tsx
@@ -0,0 +1,63 @@
+'use client'
+import { useForm, ValidationError } from '@formspree/react';
+
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm('xvojydon');
+
+  if (state.succeeded) {
+    return (
+      <div className="text-center mt-8">
+        <p className="text-green-500 text-lg">Thanks for joining!</p>
+      </div>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={state.errors}
+          className="text-red-500 text-xs"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+          className="text-red-500 text-xs"
+        />
+      </div>
+
+      <div className="text-center">
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,15 @@
+import ContactForm from "../component/ContactInfo/ContactForm";
 import Map from "../component/Map/Map";
 
 export default function Contact(){
     return(
+<div>
         <div className="mt-5 w-screen h-screen md:w-1/2 md:h-screen lg:w-1/2 lg:h-screen">
-  <Map/>
+        <Map/>
+        </div>
+        <div>
+        <ContactForm/>
+        </div>
 </div>
     )
 }
