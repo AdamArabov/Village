6c82b01b3e69b4fdf0640194b060993b93dc8e22
AdamArabov <arabovadam@yahoo.com>
2023-11-07 12:13:45 -0500

    Added Map to Contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,9 @@
+import Map from "../component/Map/Map";
+
 export default function Contact(){
     return(
-        <div>
-            
-        </div>
+        <div className="mt-5 w-screen h-screen md:w-1/2 md:h-screen lg:w-1/2 lg:h-screen">
+  <Map/>
+</div>
     )
 }
