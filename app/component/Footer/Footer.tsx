0d39617db3af9bd6a63d450688a14881b692afa4
AdamArabov <arabovadam@yahoo.com>
2023-11-06 20:49:50 -0500

    Footer Mobile Friendly

--- app/component/Footer/Footer.tsx
+++ app/component/Footer/Footer.tsx
@@ -1,53 +1,92 @@
-export default function Footer (){
-    return(
-        <footer className="text-gray-600 body-font">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <div className="sm:w-1/4 sm:text-left text-center">
-      <a className="flex title-font font-medium items-center justify-center text-gray-900">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span className="ml-3 text-xl">Tailblocks</span>
-      </a>
-    </div>
-    <div className="sm:w-1/2 text-center">
-      <span className="inline-flex justify-center">
-        <a className="text-gray-500">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-            <circle cx="4" cy="4" r="2" stroke="none"></circle>
-          </svg>
-        </a>
-      </span>
-    </div>
-    <div className="sm:w-1/4 text-center sm:text-right mt-4">
-      <p className="text-sm text-vill">
-        <span className="block">123 Main Street</span>
-        <span className="block">City, State ZIP</span>
-        <span className="block">Phone: (123) 456-7890</span>
-        <span className="block">Email: info@tailblocks.com</span>
-        <span className="block">Hours of Operation:</span>
-        <span className="block">Mon-Fri: 9:00 AM - 5:00 PM</span>
-        <span className="block">Sat-Sun: Closed</span>
-      </p>
-    </div>
-  </div>
-</footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="text-gray-600 body-font">
+      <div className="container px-2 sm:px-5 py-4 sm:py-8 mx-auto flex flex-col sm:flex-row items-center">
+        <div className="sm:w-1/4 sm:text-center text-center mb-4 sm:mb-0">
+          <a className="flex title-font font-medium items-center justify-center text-gray-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+        </div>
+        <div className="sm:w-1/2 text-center">
+          <span className="inline-flex justify-center">
+            <a className="text-gray-500">
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                stroke-linejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                stroke-linejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                stroke-linejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                strokeLinecap="round"
+                stroke-linejoin="round"
+                strokeWidth="0"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
+        </div>
+        <div className="sm:w-1/4 sm:text-center sm:text-right text-center mt-4 sm:mt-0">
+          <p className="text-sm text-vill">
+            <span className="block">123 Main Street</span>
+            <span className="block">City, State ZIP</span>
+            <span className="block">Phone: (123) 456-7890</span>
+            <span className="block">Email: info@tailblocks.com</span>
+            <span className="block">Hours of Operation:</span>
+            <span className="block">Mon-Fri: 9:00 AM - 5:00 PM</span>
+            <span className="block">Sat-Sun: Closed</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
