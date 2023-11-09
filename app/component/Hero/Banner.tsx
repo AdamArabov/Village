cd986b51103955999ac5d035354d673636979f25
AdamArabov <arabovadam@yahoo.com>
2023-11-09 13:39:24 -0500

    Made the Banner Bigger.

--- app/component/Hero/Banner.tsx
+++ app/component/Hero/Banner.tsx
@@ -6,14 +6,16 @@ export default function Banner() {
     return(
       <section>
       <div
-        className="bg-cover bg-center h-96"
+        className="bg-cover bg-center min-h-[570px] h-auto w-99% mx-auto"
         style={{
           backgroundImage: 'url(https://lh3.googleusercontent.com/p/AF1QipMF4Ol0QJx45FzdCodAwPQJTHcBvmOMZlGM1BV7=s1360-w1360-h1020)',
+          
           margin: '50px',
           backgroundColor: '#ffffff',
+          
         }}
       >
-        <div className="bg-black bg-opacity-40 h-full flex flex-col items-center justify-center text-white">
+        <div className="bg-black bg-opacity-40 h-full flex flex-col items-center justify-center text-white min-h-[570px] h-auto w-99% mx-auto">
           <div className="text-4xl text-center font-bold tracking-wide mb-4">Village Square Pharmacy</div>
           <div className="text-2xl text-center font-bold mb-4">Prescription for Wellness, Care You Can Trust</div>
           <div className="mb-4 text-center">
