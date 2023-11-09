cb346d51a7e98e233a4992cb6ce5fe20a73719f5
AdamArabov <arabovadam@yahoo.com>
2023-11-09 14:38:03 -0500

    Update hours contact

--- app/component/ContactInfo/Operation.tsx
+++ app/component/ContactInfo/Operation.tsx
@@ -1,9 +1,9 @@
 export default function Operations() {
     return (
       <div className="text-center">
-        <h2 className="text-2xl font-bold">Hours and Days of Operations</h2>
+        <h2 className="text-2xl font-bold">Hours of Operations</h2>
         <p className="text-lg">Monday - Friday: 9:00 AM - 6:00 PM</p>
-        <p className="text-lg">Saturday: 10:00 AM - 4:00 PM</p>
+        <p className="text-lg">Saturday: 9:00 AM - 5:00 PM</p>
         <p className="text-lg">Sunday: Closed</p>
       </div>
     );
