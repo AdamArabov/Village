ffebb2199611f9f4c4e724bab2ec19c7fe132ef8
AdamArabov <arabovadam@yahoo.com>
2023-11-09 20:43:37 -0500

    Spacing

--- app/component/ServiceInfo/Service.tsx
+++ app/component/ServiceInfo/Service.tsx
@@ -21,7 +21,9 @@ export default function Service() {
               backgroundAttachment: 'fixed',
               backgroundSize: 'cover',
             }}
-          ></div>
+          >
+            
+          </div>
             <div className="service-content absolute inset-0 flex items-center justify-center bg-opacity-50 bg-gray-200">
             <div className="bg-white bg-opacity-50 p-8 rounded-lg shadow-lg text-center w-full max-w-xs mx-auto">
               <h1 className="text-4xl font-bold mb-4">{service.name}</h1>
