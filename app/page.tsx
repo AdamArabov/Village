4663bd34a68daba707c589fc439999887f0c7db4
AdamArabov <arabovadam@yahoo.com>
2023-11-17 12:48:42 -0500

    MetaSeo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ import About from "./component/Hero/About";
 import Banner from "./component/Hero/Banner";
 import Services from "./component/Hero/Services";
 import Map from "./component/Map/Map";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Village Square Pharmacy",
+    openGraph: {
+      images: [
+        '/Photo (14).jpg',
+      ],
+    },
+  };
+}
 
 export default function Home() {
   return (
